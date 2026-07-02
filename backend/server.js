f7d8828cf75f2c1a738cadec85bb230f9067e934
Arpit Gupta <arpitgupta0406@gmail.com>
2026-07-02 13:37:55 +0530

    progress

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || '*',
+    origin: process.env.CLIENT_URL || 'inventory-management-irw0o8393-arpit168s-projects.vercel.app'||"*",
     credentials: true,
   })
 );
