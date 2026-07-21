553fcc13b1483c328a36c2a164ffb05866c38733
Arpit Gupta <arpitgupta0406@gmail.com>
2026-07-21 20:47:17 +0530

    done

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://inventory-management-7oq4.onrender.com" ,
+  baseURL: import.meta.env.VITE_API_URL || "https://inventory-management-7oq4.onrender.com/api" ,
   timeout: 20000,
 });
 
